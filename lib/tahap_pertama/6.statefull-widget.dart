d34029a4f6c4583cacadb516c999b91a4cc80dc9
Rafa Khadafi <rafakhadafi29@gmail.com>
2025-07-27 14:32:00 +0700

    stateful-widget

--- lib/tahap_pertama/6.statefull-widget.dart
+++ lib/tahap_pertama/6.statefull-widget.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 // maka kalo dia di panggil, maka hanya akan rebuild widget itu saja
 
 // https://chatgpt.com/c/6885bb99-df10-8009-ac93-1702161d3f30
-
+// https://chatgpt.com/c/6885bb99-df10-8009-ac93-1702161d3f30
 
 // jadi stateFull widget ini berfungsi
 // untuk melakukan perubahan ketika ada perubahan
@@ -34,7 +34,6 @@ import 'package:flutter/material.dart';
 //   }
 // }
 
-
 // Ketika runApp() dijalankan, Flutter buat Element tree.
 // Jika menemukan StatefulWidget, dia:
 // buat instance dari StatefulWidget
@@ -43,9 +42,6 @@ import 'package:flutter/material.dart';
 // Ketika ada perubahan (misal tombol diklik), panggil setState().
 // setState() memberi tahu Flutter bahwa state berubah.
 // Flutter memanggil kembali build(), dan hanya widget itu yang dibangun ulang, bukan seluruh app.
-
-
-
 
 // jadi fungsi dari method createState ini adalh
 // untuk menggabungkan antara widget dengan statenya
@@ -62,7 +58,6 @@ import 'package:flutter/material.dart';
 // artinya class ini adalah sebuah state yang bsia breubha ubah
 // dan berubah ketika si setState dipanggil
 // dan hanya class ini saja yang akan di rebuild
-
 
 // atau gini
 // 1️⃣ Pertama, kita buat class yang extends StatefulWidget.
@@ -88,30 +83,39 @@ import 'package:flutter/material.dart';
 //     - State (`State<>`) bersifat dinamis dan bisa berubah.
 //     - Setiap kali ada perubahan melalui setState(), **yang dibangun ulang adalah build() dari State tersebut saja**.
 
-
-
 // CONTOHNYA
 // disini saya ignin agar appbar saya itu stateless
 // dan body nya itu stateful
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
 // berati pertama kita baut dulu statelessnya
-// untuk MyApp 
+// untuk MyApp
 class MyApp extends StatelessWidget {
-  @override 
-  Widget build(BuildContext context){
+  @override
+  Widget build(BuildContext context) {
     // jadi build context ini sebagai pengenal untuk widget ini
     // ketika nanti di taruh di tree widget
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title:Text("ini adalah appbar")),
+        appBar: AppBar(title: Text("ini adalah appbar")),
         // jadi di bodynnya kita baut widget
         // yang bisa berubah ubah
-        body: MyStateFulWidgetNya()
-      )
+        body: Container(
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 176, 80, 141),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [MyStateFulWidgetNya()],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
@@ -120,17 +124,91 @@ class MyApp extends StatelessWidget {
 // kemudian state dari class widgetStatefulnya
 
 class MyStateFulWidgetNya extends StatefulWidget {
-   @override
-   _MyStateNya createState(){
-      return _MyStateNya();
-   }
+  @override
+  _MyStateNya createState() {
+    return _MyStateNya();
+  }
 }
 
 class _MyStateNya extends State<MyStateFulWidgetNya> {
+  // nah disini kita buat fungsinya
+
+  // nah disni kita buat field
+  // nah dia ini yang akan di pake di hasil
+  int number = 0;
+
+  void tambahAngka() {
+
+    // nah adi setState ini akan di panggil ketika di klik
+    // dan ketika setState ini di panggil otomatis dia akan rebuild / reload lagi
+    // tapi hanya widget ini saja yang akan di rebuild
+    // jadi ketika kita panggil fungsi ini
+    // maka dia akan menambah angka
+    setState(() {
+      number++;
+    });
+  }
+
+  // JADI SETSTATE INI ADALAH PERINTAH UNTUK REBUILD / REFRESH
+
+  void kurangAngka() {
+    setState(() {
+      number--;
+    });
+  }
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     // nah disini tinggal kita lanjutin dari yang si body dari si MyAppnya
-    return Center();
+    return Container(
+      padding: EdgeInsets.fromLTRB(2.0, 100.0, 2.0, 50.0),
+      width: 300.0,
+      height: 300.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        border: Border.all(
+          color: const Color.fromARGB(255, 247, 246, 246),
+          width: 5.0,
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey,
+            blurRadius: 5.0,
+            offset: Offset(2.0, 2.0),
+          ),
+        ],
+        gradient: LinearGradient(
+          colors: [Colors.red, Colors.blue],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              // agar ukuran ntestnya berubah sesuai number
+              '${this.number.toString()}',
+              style: TextStyle(fontSize: 10 + this.number.toDouble(), color: Colors.white),
+            ),
+            // disni kita buat buttonnya
+            Container(
+              child: ElevatedButton(
+                child: Text('Tambah'),
+                onPressed: tambahAngka,
+              ),
+            ),
+            Container(
+              child: ElevatedButton(
+                child: Text('Kurang'),
+                onPressed: kurangAngka,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
 
